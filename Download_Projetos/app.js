23f6c587b9392492561b3934b233b99e86210c2f
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-01-21 20:23:34 -0300

    implementa primeiro desafio do modulo 2

---
+++ Download_Projetos/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha o um número entre 1 e 10';
+// tem responsabilidade de realizar alguma ação. tem de ter uma responsabilidade única
+function verificarChute() {
+
+    console.log('O botão foi clicado')
+}
