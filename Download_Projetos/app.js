34542ba9c88a71965185a778d30d8540d61ca706
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-01-22 20:29:05 -0300

    cria os arquivos do exercicio de funcao

--- Download_Projetos/app.js
+++ Download_Projetos/app.js
@@ -1,3 +1,6 @@
+
+let numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto) {
     let campos = document.querySelector(tag);
     campos.innerHTML = texto;
@@ -6,9 +9,16 @@ function exibirTextoNaTela(tag, texto) {
 exibirTextoNaTela('h1', 'Jogo do Número Secreto');
 exibirTextoNaTela('p', 'Escolha o um número entre 1 e 10');
 
-
+console.log(numeroSecreto);
 // tem responsabilidade de realizar alguma ação. tem de ter uma responsabilidade única
-function verificarChute() {
-
-    console.log('O botão foi clicado');
+function verificarChute() { // sem retorno
+    console.log(numeroSecreto);
+    
+    let inputNumero = document.querySelector('input').value;
+    inputNumero == numeroSecreto ? console.log('Você acertou') : console.log('Você errou.'); 
+    // console.log(numeroSecreto);
+}
+// sem parametro com retorno
+function gerarNumeroAleatorio() {
+   return parseInt(Math.random() * 10 + 1);
 }
