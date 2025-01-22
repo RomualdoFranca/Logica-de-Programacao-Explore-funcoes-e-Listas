ca3427bb637863c54aac7065b6d0a5bdc4d95fb2
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-01-22 15:10:39 -0300

    tentativa de implementar uma funçao que altera um texto de uma div

--- My_Ideas/NumeroSecreto1.2/app.js
+++ My_Ideas/NumeroSecreto1.2/app.js
@@ -10,5 +10,13 @@ inserirTexto('h1', 'Calculadora Simples');
 
 //função que efetua a soma
 function efetuaSoma(){
-    console.log('O botão da soma foi clicado');
+    const div = document.getElementById('display');
+    const h5 = div.querySelector('h5');
+    h5.textContent = prompt('Digite dois números para realizar a soma.');
 }
+
+// function textoDisplay(){
+//     const textoDisplay = document.getElementById("textoDisplay");
+//     textoDisplay.textContent = efetuaSoma;
+
+// }
