400480f09d305b53fea0a529260cc97c74df36ff
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-01-22 21:29:25 -0300

    implementa e testa a funcao do exercicio 3

--- Desafios/desafio3/app.js
+++ Desafios/desafio3/app.js
@@ -11,3 +11,8 @@ function exibirMensagemComNome(nome){
 exibirMensagemComNome('Lua');
 
 // criar uma função que recebe um número como parâmetro e retorna o dobro desse número
+function dobraNumero(numero) {
+    let dobro = 2 * numero;
+    console.log(`O dobro de ${numero} é ${dobro}`);
+}
+dobraNumero(10);
