62c3ba31f9c5344a68fe57bdb6e890400c14ffa6
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-03-01 21:04:13 -0300

    implementa funcao que altera o estilo da pagina

--- My_Ideas/Events/app.js
+++ My_Ideas/Events/app.js
@@ -1,9 +1,19 @@
 
 let numeroLimite = 10;
 let numeroSecreto = geraNumeroAleatorio();
+console.log("Numero secreto: " + numeroSecreto);
 
 
+
+
+function geraNumeroAleatorio() {
+    let numero = parseInt(Math.random() * numeroLimite + 1);
+    return numero;
+}
+
+alterarEstilo("main-section", "button-chute", "modo-escuro");
 function verificaChute() {
+
     let chute = document.querySelector("input").value;
     if(chute == numeroSecreto) {
         console.log("Tudo permance igual")
@@ -13,10 +23,33 @@ function verificaChute() {
     }
 }
 
-function geraNumeroAleatorio() {
-    let numero = parseInt(Math.random() * numeroLimite + 1);
-    return numero;
+function alterarEstilo(seletorElemento, seletorBotao, classe) {
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const elemento = document.getElementById(seletorElemento);
+        const botao = document.getElementById(seletorBotao);
+        if (elemento) {
+            botao.addEventListener("click", () => {
+                elemento.classList.toggle(classe);
+            });
+        }
+    });
 }
 
-console.log("Numero secreto: " + numeroSecreto);
+// function toggleClass(elemento, classe) {
+//     if(elemento) {
+//         elemento.classList.toggle(classe);
+//     }
+// }
+// document.addEventListener("DOMContentLoaded", () => {
+//     const conteudoPrincipal = document.getElementById("main-section");
+//     const butaoChute = document.getElementById("button-chute");
+
+//     butaoChute.addEventListener("click", () => {
+//         toggleClass(conteudoPrincipal, "modo-escuro");
+//     });
+
+// });
+
+
 
